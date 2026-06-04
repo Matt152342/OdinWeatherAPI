78d5ed018d6881ea0ce19fca875c5d32b526929d
Matthew van Heerden <matt.v.h152342@gmail.com>
2026-06-04 22:15:34 +0200

    Created function to process data into one object

--- src/index.js
+++ src/index.js
@@ -5,19 +5,37 @@ api.town = "George";
 api.country = "South Africa";
 api.metric = "metric";
 
-let finalURL = api.finalURL;
-const getData = async () => {
+const rawData = async () => {
     try {
-        const response = await fetch(finalURL);
+        const response = await fetch(api.finalURL);
         if (!response.ok) {
             throw new Error(`Response status: ${response.status}`)
         }
 
         const result = await response.json();
-        console.log(result);
+        return result;
     } catch (error) {
         console.error(error.message);
     }
 }
 
-getData();
+const processData = async () => {
+    try {
+        const data = await rawData();
+
+        const processed = {
+            conditions: data.currentConditions.conditions,
+            time: data.currentConditions.datetime,
+            humidity: data.currentConditions.humidity,
+            temperature: data.currentConditions.temp,
+        };
+
+        return processed;
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+processData().then((response) => {
+    console.log(response.conditions);
+});
